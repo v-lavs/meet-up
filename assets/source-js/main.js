0dabaf32c5db3f9711e8161648fb9d002a9ad7d6
v-lavs <lavskavanda@gmail.com>
2018-08-04 17:04:10 +0300

    ready-made work is added

---
+++ assets/source-js/main.js
@@ -0,0 +1,73 @@
+jQuery(document).ready(function () {
+
+  //mob menu
+  var nav = jQuery ('.header-menu');
+  jQuery('.btn-burger').click(function (e) {
+    e.preventDefault();
+    nav.addClass('open');
+  });
+
+  jQuery('.btn-close').click(function (e) {
+    e.preventDefault();
+    nav.removeClass('open');
+  });
+
+//coutdawn
+
+  var countDownDate = new Date("Oct 20, 2018 23:07:05").getTime();
+
+  var daysTag = document.getElementById("days");
+  var hoursTag = document.getElementById("hours");
+  var minutesTag = document.getElementById("minutes");
+  var secondsTag = document.getElementById("seconds");
+
+  var x = setInterval(function() {
+
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    daysTag.innerHTML = days;
+    hoursTag.innerHTML = hours;
+    minutesTag.innerHTML = minutes;
+    secondsTag.innerHTML = seconds;
+
+    if (distance < 0) {
+      clearInterval(x);
+      document.getElementById("countdown").innerHTML = "EXPIRED";
+    }
+  }, 1000);
+
+  //accordeon
+  jQuery('.panel-heading').click(function (e) {
+    e.preventDefault();
+    jQuery(this).toggleClass('open').next().slideToggle();
+  });
+
+  //header scroll
+  var scrolled;
+  window.onscroll = function() {
+    scrolled = window.pageYOffset || document.documentElement.scrollTop;
+    if(scrolled > 100){
+      jQuery(".header-site").css({"background": "#373737"})
+    }
+    if(100 > scrolled){
+      jQuery(".header-site").css({"background": "transparent"})
+    }
+  };
+
+//  scrolling
+  jQuery("#menu").on("click","a", function (e) {
+    e.preventDefault();
+    var id  = jQuery(this).attr('href'),
+      top = jQuery(id).offset().top;
+    jQuery('body,html').animate({scrollTop: top}, 3000);
+  });
+
+});
+
+
+
---
+++ gulpfile.js
@@ -0,0 +1,61 @@
+var gulp = require('gulp'),
+    sourcemaps = require('gulp-sourcemaps'),
+    plumber = require('gulp-plumber'),
+    autoprefixer = require('gulp-autoprefixer'),
+    watch = require('gulp-watch'),
+    rename = require('gulp-rename'),
+    notify = require('gulp-notify'),
+    sass = require('gulp-sass'),
+    gulpif = require('gulp-if'),
+    gcmq = require('gulp-group-css-media-queries'),
+    cssmin = require('gulp-cssmin');
+
+var config = {
+    sourceMaps: true,
+    sassOptions: {
+        errLogToConsole: true,
+        precision: 4,
+        noCache: true,
+    }
+};
+
+
+// Default error handler
+var onError = function (err) {
+    console.log('An error occured:', err.message);
+    this.emit('end');
+};
+
+gulp.task('sass', function () {
+    return gulp.src('./assets/scss/main.scss')
+        .pipe(plumber({errorHandler: onError}))
+        .pipe(sourcemaps.init())
+        .pipe(sass(config.sassOptions))
+        .pipe(sourcemaps.write('./map'))
+        .pipe(gulp.dest('./assets/css'))
+        .pipe(notify({title: 'Styles', message: 'update successfully'}));
+});
+
+gulp.task('sass:build', function () {
+    return gulp.src('./assets/scss/screen.scss')
+        .pipe(plumber({errorHandler: onError}))
+        .pipe(sass(config.sassOptions))
+        .pipe(autoprefixer({
+            browsers: ['last 6 version', '> 1%', 'ie 9']
+        }))
+        .pipe(gcmq())
+        .pipe(cssmin())
+        .pipe(gulp.dest('./assets/css'))
+        .pipe(notify({title: 'Styles', message: 'build successfully'}));
+});
+
+// watch files for change
+gulp.task('watch', ['sass'], function () {
+    gulp.watch('./assets/scss/**/*.scss', ['sass']);
+});
+
+gulp.task('build', ['sass:build'], function () {
+});
+
+gulp.task('default', ['sass'], function () {
+});
